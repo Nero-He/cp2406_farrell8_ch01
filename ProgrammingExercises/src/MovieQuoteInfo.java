7c188edfc7477363cf177c10031f4952f7a899be
Nero-He <81955576+Nero-He@users.noreply.github.com>
2021-11-10 21:07:42 +0800

    Add files via upload
    Programing Exercise

---
+++ ProgrammingExercises/src/MovieQuoteInfo.java
@@ -0,0 +1,8 @@
+public class MovieQuoteInfo {
+    public static void main(String[] args){
+        System.out.println("Soul.");
+        System.out.println("Pixar.");
+        System.out.println("2020.");
+        System.out.println("Peter Hans Docter.");
+    }
+}
